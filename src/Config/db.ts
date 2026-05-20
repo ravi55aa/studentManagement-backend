6a6bb2df42f163a53f05e70030aea153156d0d7d
Ravi <raviaa912@gmail.com>
2026-05-20 09:44:00 +0530

    docker.host.db over local_MongoDB server for dockerization

--- src/Config/db.ts
+++ src/Config/db.ts
@@ -1,10 +1,13 @@
 import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 import logger from '../Utils/logger';
 
 const mongoDB = async () => {
   try {
-    await mongoose.connect('mongodb://127.0.0.1:27017/secondProject');
+    await mongoose.connect(process.env.MONGO_URI!);
     logger.info('[Database] connected 📊🔐');
   } catch (err) {
     throw new Error('Database error', { cause: err });
